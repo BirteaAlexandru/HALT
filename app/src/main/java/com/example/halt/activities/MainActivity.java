a1b0941238f45448de0064ce1dc6d2b1149ebe34
Alex <alexbirta124@gmail.com>
2021-10-25 11:50:15 +0300

    git in

---
+++ app/src/main/java/com/example/halt/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.halt.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.halt.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/halt/constants/Constants.java
@@ -0,0 +1,19 @@
+package com.example.halt.constants;
+
+public class Constants {
+    public final static String EMAIL_ERROR_MESSAGE = "Enter your email";
+    public final static String PASSWORD_ERROR_MESSAGE = "Enter your password";
+    public final static String CONFIRM_PASSWORD_ERROR_MESSAGE = "Confirm your password";
+    public final static String PASSWORDS_NOT_MATCHING_ERROR_MESSAGE = "Passwords do not match";
+    public final static String INVALID_EMAIL_ERROR_MESSAGE = "Email is invalid !";
+    public final static String PROGRESS_DIALOG_MESSAGE = "Please wait...";
+    public final static String SUCCESSFUL_REGISTER_MESSAGE = "Successfully registered";
+    public final static String FAILED_REGISTER_ERROR_MESSAGE = "Sign up failed";
+    public final static String FAILED_LOGIN_ERROR_MESSAGE = "Login failed";
+    public final static String SUCCESSFUL_LOGIN_MESSAGE = "Login successful";
+    public final static String PASSWORD_LENGTH_ERROR_MESSAGE = "Password's length should be higher than or equal to 6";
+    public final static String SUCCESSFUL_CHANGE_PASSWORD_MESSAGE = "E-mail for resetting password has been successfully sent !";
+    public final static String FAILED_CHANGE_PASSWORD_ERROR_MESSAGE = "E-mail for resetting password couldn't be sent !";
+    public final static String SHARED_PREFERENCES_USER_EMAIL = "UserEmail";
+
+}
